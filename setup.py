7bc45ff03a01f53412cc18c41d6b720d5e9a8aa4
IanChar <ianchar4@gmail.com>
2021-07-12 22:52:01 -0400

    Fixed setup.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/uncertainty-toolbox/deep-uq-baselines",
     packages=setuptools.find_packages(),
-    # install_requires=requirements,
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
